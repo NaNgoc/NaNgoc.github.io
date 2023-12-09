84bf1ef56b7842ca263a4135f452e6074be98098
NaNgoc <jnguyen394@gatech.edu>
2023-12-08 20:48:05 -0500

    wooh

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ window.addEventListener('scroll', () => {
         nameContainer.classList.add('name-sticky');
     }
 });
+
+// Get the modal
+var modal = document.getElementById("imageModal");
+
+// Get the image and insert it inside the modal
+var img = document.querySelectorAll('.enlargeImage');
+var modalImg = document.getElementById("img01");
+
+img.forEach(function(image) {
+    image.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
